176b1ae9f7c5ce23a11a86fa35b1be8e09e863f8
Tiago de Pádua <tiago.padua@hp.com>
2018-04-16 12:27:41 -0300

    added lunch time message & fixed time calculation

--- src/js/ast.js
+++ src/js/ast.js
@@ -5,6 +5,8 @@ const AST_MINUTE = 15;
 const AST_SECONDS = ((AST_HOUR * 60) + AST_MINUTE) * 60;
 const DAY_IN_SECONDS = 24 * 60 * 60;
 
+const AST_LENGTH = 60 * 60; // 60 minutes
+
 function format_seconds(total_seconds) {
     let result = {
         hours: Math.abs(Math.floor(total_seconds / 3600)),
@@ -14,25 +16,39 @@ function format_seconds(total_seconds) {
     return result.hours.toString().padStart(2, "0") + ":" + result.minutes.toString().padStart(2, "0") + ":" + result.seconds.toString().padStart(2, "0");
 }
 
+function ast_difference(hour, minute, second) {
+    let total_seconds = ((hour * 60) + minute) * 60 + second;
+    let difference = AST_SECONDS - total_seconds;
+
+    if (difference < 0) {
+        difference += DAY_IN_SECONDS;
+    }
+
+    return difference;
+}
+
 window.ast = new Vue({
     el: "#ast",
     data: {
-        time: null
+        time: null,
+        show_minus: true
     },
     methods: {
         updateTime: function() {
             let now = new Date();
             let hour = now.getHours(),
                 minute = now.getMinutes(),
-                seconds = now.getSeconds();
+                second = now.getSeconds();
 
-            let total_seconds = ((hour * 60) + minute) * 60 + seconds;
-            let difference = total_seconds - AST_SECONDS;
+            let difference = ast_difference(hour, minute, second);
 
-            if (difference > 0) {
-                difference -= DAY_IN_SECONDS;
+            if (difference === 0 || difference > (DAY_IN_SECONDS - AST_LENGTH)) {
+                this.time = "Lunch time!";
+                this.show_minus = false;
+                return;
             }
 
+            this.show_minus = true;
             this.time = format_seconds(difference);
         }
     }
